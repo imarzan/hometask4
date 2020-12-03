96a4cc0630567ebbc55a12ce62c8f3a524d51b78
Iryna Marzan <iryna_marzan@epam.com>
2020-12-03 12:38:54 +0300

    Revert "file names case was changed"
    This reverts commit 6d959cd83f459f3b9e730cf5d3c0aecd0c931194.

--- JS/models/experimentalTypes.js
+++
@@ -1,8 +0,0 @@
-const ExperimentalTypes = {
-    LIFTING_BODY: 'Lifting_Body',
-    HYPERSONIC: 'Hypersonic',
-    HIGH_ALTITUDE: 'High_Altitude',
-    VTOL: 'Vtol'
-};
-
-module.exports = ExperimentalTypes;
